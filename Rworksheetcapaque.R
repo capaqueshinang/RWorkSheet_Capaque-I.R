049d375a18993e873e97e279d74e273732dd9d1e
ShaneCapaque <shanecapaquegmail.com>
2023-09-20 04:14:19 +0000

    Shane

---
+++ Rworksheetcapaque.R
@@ -0,0 +1,26 @@
+Age <-c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+ 
+length (Age)
+
+reciprocal_age <-1/Age
+    reciprocal_age
+
+#new_Age
+    new_Age <-c(Age,0,Age)
+    new_Age
+
+#sort_the_values_for_Age
+    sorted_Age <- sort(Age)
+    sorted_Age
+
+max(Age)
+min(Age)
+
+Age_Vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length(Age_Vec)
+
+
+
+
+    
